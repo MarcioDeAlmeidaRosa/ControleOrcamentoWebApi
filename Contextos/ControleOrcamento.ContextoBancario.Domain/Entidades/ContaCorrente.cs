d1a344798c2ec06be956191a4fd2b9e9b5186e72
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-11-21 20:57:23 -0200

    Inclusão de novos campos na conta corrente

--- Contextos/ControleOrcamento.ContextoBancario.Domain/Entidades/ContaCorrente.cs
+++ Contextos/ControleOrcamento.ContextoBancario.Domain/Entidades/ContaCorrente.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ControleOrcamento.ContextoBancario.Domain.Enums;
 using ControleOrcamento.Contexto.Domain.Entidade.Contratos.Usuario;
 
 namespace ControleOrcamento.ContextoBancario.Domain.Entidades
@@ -29,24 +30,58 @@ namespace ControleOrcamento.ContextoBancario.Domain.Entidades
         public string DV { get; private set; }
 
         /// <summary>
+        /// Quando verdadeiro a conta principal do usuário para lançamentos automáticos
+        /// </summary>
+        public string EhPrincipal { get; set; }
+
+        /// <summary>
+        /// Quando verdadeiro a aplicação controla o saldo na conta do usuário com os lancamentos de débitos/créditos
+        /// </summary>
+        public string ControlaSaldo { get; set; }
+
+        /// <summary>
+        /// Representa o tipo de conta do usuário da aplicação
+        /// </summary>
+        public TipoContaCorrente Tipo { get; set; }
+
+        /// <summary>
+        /// Representa o valor do saldo da conta corrente no mundo real
+        /// </summary>
+        public decimal Saldo { get; private set; }
+
+        /// <summary>
+        /// Representa o valor do limite da conta corrente no mundo real
+        /// </summary>
+        public decimal Limite { get; private set; }
+
+        /// <summary>
         /// Cria nova conta corrente garantindo seu estado válido
         /// </summary>
         /// <param name="usuario">Usuário responsável pela conta corrente</param>
         /// <param name="agencia">Agência da conta corrente</param>
         /// <param name="numero">Número da conta corrente</param>
         /// <param name="dv">DV da conta corrente</param>
+        /// <param name="tipo">Tipo da conta corrente</param>
+        /// <param name="saldo">Saldo da conta corrente</param>
+        /// <param name="limite">Limite da conta corrente</param>
         /// <exception cref="ArgumentNullException">Lançado quando não for informado qualquer das propriedades:
         /// <paramref name="usuario"/>
         /// <paramref name="agencia"/>
         /// <paramref name="numero"/>
         /// <paramref name="dv"/>
+        /// <paramref name="tipo"/>
+        /// <paramref name="saldo"/>
+        /// <paramref name="limite"/>
         /// </exception>
-        public ContaCorrente(UsuarioBase usuario, Agencia agencia, string numero, string dv)
+        public ContaCorrente(UsuarioBase usuario, Agencia agencia, string numero, string dv, TipoContaCorrente? tipo, decimal? saldo, decimal? limite)
         {
             Usuario = usuario ?? throw new ArgumentNullException(nameof(usuario), "Não informado usuário da conta corrente");
             Agencia = agencia ?? throw new ArgumentNullException(nameof(agencia), "Não informada agência da conta corrente");
             Numero = numero ?? throw new ArgumentNullException(nameof(numero), "Não informado número da conta corrente");
             DV = dv ?? throw new ArgumentNullException(nameof(dv), "Não informado o DV da conta corrente");
+            Tipo = tipo ?? throw new ArgumentNullException(nameof(tipo), "Não informado o tipo da conta corrente");
+            saldo = saldo ?? throw new ArgumentNullException(nameof(saldo), "Não informado o saldo da conta corrente");
+            limite = limite ?? throw new ArgumentNullException(nameof(limite), "Não informado o limite da conta corrente");
         }
 
         /// <summary>
---
+++ Contextos/ControleOrcamento.ContextoBancario.Domain/Enums/TipoContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿namespace ControleOrcamento.ContextoBancario.Domain.Enums
+{
+    /// <summary>
+    /// Representa o tipo de conta corrente no mundo real
+    /// </summary>
+    public enum TipoContaCorrente
+    {
+        /// <summary>
+        /// Representa uma conta corrente do mundo real
+        /// </summary>
+        Corrente,
+        /// <summary>
+        /// Representa uma conta poupança do mundo real
+        /// </summary>
+        Poupanca,
+        /// <summary>
+        /// Representa uma conta salário do mundo real
+        /// </summary>
+        Salario,
+    }
+}
