bd016e4cb8a7e34750aa3bbb7564cd508a80f8e5
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-11-21 23:39:34 -0200

    Inclusão do contexto de classificação

--- build.ps1
+++ build.ps1
@@ -33,6 +33,11 @@ Write-Output "###### -> msbuild Restore -> ControleOrcamento.ContextoBancario.Do
 msbuild /t:Restore,Pack .\Contextos\Testes\ControleOrcamento.ContextoBancario.Domain.Teste\ /p:targetFrameworks='"netcoreapp2.1"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
+	
+Write-Output "###### -> msbuild Restore -> ControleOrcamento.ContextoClassificacao.Domain.Teste"
+msbuild /t:Restore,Pack .\Contextos\Testes\ControleOrcamento.ContextoClassificacao.Domain.Teste\ /p:targetFrameworks='"netcoreapp2.1"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
 Write-Output "###### -> msbuild Restore -> ControleOrcamento.ContextoQuitacao.Domain.Teste"
 msbuild /t:Restore,Pack .\Contextos\Testes\ControleOrcamento.ContextoQuitacao.Domain.Teste\ /p:targetFrameworks='"netcoreapp2.1"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
@@ -51,6 +56,11 @@ if (-Not $LastExitCode -eq 0)
 
 Write-Output "###### -> msbuild Restore -> ControleOrcamento.ContextoBancario.Domain"
 msbuild /t:Restore,Pack .\Contextos\ControleOrcamento.ContextoBancario.Domain\ /p:targetFrameworks='"netcoreapp2.1"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
+ if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
+	
+Write-Output "###### -> msbuild Restore -> ControleOrcamento.ContextoClassificacao.Domain"
+msbuild /t:Restore,Pack .\Contextos\ControleOrcamento.ContextoClassificacao.Domain\ /p:targetFrameworks='"netcoreapp2.1"' /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile /p:ProductVersion=$versionProduct /p:Configuration=Release /p:IncludeSymbols=true /p:PackageOutputPath=..\..\artifacts /verbosity:minimal
  if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 	
