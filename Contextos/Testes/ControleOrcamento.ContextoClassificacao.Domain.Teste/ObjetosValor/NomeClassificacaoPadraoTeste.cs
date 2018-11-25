0730ee11cca106427e84c6568d180df08806ec12
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-11-25 03:17:15 -0200

    Ajustando testes

---
+++ Contextos/Testes/ControleOrcamento.ContextoClassificacao.Domain.Teste/ObjetosValor/NomeClassificacaoPadraoTeste.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControleOrcamento.ContextoClassificacao.Domain.ObjetosValor;
+
+namespace ControleOrcamento.ContextoClassificacao.Domain.Teste.ObjetosValor
+{
+    [TestClass]
+    public class NomeClassificacaoPadraoTeste
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Moradia")]
+        [DataRow("Transporte")]
+        [DataRow("Saúde")]
+        [DataRow("Alimentação")]
+        [DataRow("Filhos")]
+        [DataRow("Educação")]
+        [DataRow("Obrigações bancárias")]
+        [DataRow("Entretenimento")]
+        [DataRow("Cuidados pessoais")]
+        [DataRow("Animais domésticos")]
+        [DataRow("Presentes e doações")]
+        [DataRow("Poupança/Investimentos")]
+        [DataRow("Salário")]
+        public void DeveRetornarNomeValido(string nome)
+        {
+            var objeto = new NomeClassificacaoPadrao(nome);
+            Assert.IsTrue(!objeto.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("P")]
+        [DataRow("Po")]
+        [DataRow("Pou")]
+        [DataRow("Poup")]
+        public void DeveRetornarNomeInvalido(string nome)
+        {
+            var objeto = new NomeClassificacaoPadrao(nome);
+            Assert.IsTrue(objeto.Invalid);
+        }
+    }
+}
---
+++ Contextos/Testes/ControleOrcamento.ContextoClassificacao.Domain.Teste/ObjetosValor/NomeLancamentoPadraoTeste.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControleOrcamento.ContextoClassificacao.Domain.ObjetosValor;
+
+namespace ControleOrcamento.ContextoClassificacao.Domain.Teste.ObjetosValor
+{
+    [TestClass]
+    public class NomeLancamentoPadraoTeste
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Aluguel")]
+        [DataRow("Taxa do condomínio")]
+        [DataRow("Financiamento do imóvel")]
+        [DataRow("Impostos do imóvel")]
+        [DataRow("Remoção de lixo")]
+        [DataRow("Manutenção/ reforma")]
+        [DataRow("Seguro")]
+        [DataRow("Luz")]
+        [DataRow("Água/ esgoto")]
+        [DataRow("Telefone")]
+        [DataRow("Tv por assinatura")]
+        [DataRow("Netflix")]
+        [DataRow("Celular")]
+        public void DeveRetornarNomeValido(string nome)
+        {
+            var objeto = new NomeLancamentoPadrao(nome);
+            Assert.IsTrue(!objeto.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("P")]
+        [DataRow("Po")]
+        public void DeveRetornarNomeInvalido(string nome)
+        {
+            var objeto = new NomeClassificacaoPadrao(nome);
+            Assert.IsTrue(objeto.Invalid);
+        }
+    }
+}
