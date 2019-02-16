3ee11b25c3472cc228de687a0a1379943824d440
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2019-02-16 13:13:47 -0200

    Continuação do contexto bancário

---
+++ Contextos/Testes/ControleOrcamento.ContextoBancario.Domain.Teste/Mocks/FakeContaCorrente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ControleOrcamento.ContextoBancario.Domain.Servicos;
+
+namespace ControleOrcamento.ContextoBancario.Domain.Teste.Mocks
+{
+    public class FakeContaCorrente : IServicoContaCorrente
+    {
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("Valor para depósito não é válido", nameof(valor));
+            }
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("Valor para saque não é válido", nameof(valor));
+            }
+        }
+    }
+}
